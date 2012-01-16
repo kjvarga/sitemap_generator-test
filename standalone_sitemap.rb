4f846f45b970e77308d48c157ab5cd24d364a362
Karl Varga <kjvarga@gmail.com>
2012-01-15 23:46:02 -0800

    Add a sitemap config which can be run as a script using rubygems

---
+++ standalone_sitemap.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'sitemap_generator'
+
+SitemapGenerator::Sitemap.verbose = true
+SitemapGenerator::Sitemap.default_host = 'http://example.com'
+SitemapGenerator::Sitemap.create do
+  add '/ruby', :lastmod => Time.now, :changefreq => 'always', :priority => 1.0
+end
