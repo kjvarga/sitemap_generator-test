a8ac18806b406796706a9bef0a6d44160f4740b4
Karl Varga <kjvarga@gmail.com>
2012-01-17 19:16:22 -0800

    Script to test each type of install on all the rubies

---
+++ test_rubies.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Run the specs across different rubies.
+set -o verbose
+for branch in 'rails2' 'rails3' 'standalone'
+do
+  eval "git co ${branch}"
+  echo 'Bundling...'
+  RBXOPT="-Xrbc.db" rvm ree,1.9.2,1.9.3 exec bundle
+done   
+echo -e 'Finished bundling.\n\n\n\n\n\n\nRunning Specs...'
+for branch in 'rails2' 'rails3' 'standalone'
+do
+  eval "git co ${branch}"
+  echo 'Running specs...'
+  RBXOPT="-Xrbc.db" rvm ree,1.9.2,1.9.3 exec bundle exec rake spec
+done
+echo 'Switching to master'
+git co master
+echo 'Done.'
